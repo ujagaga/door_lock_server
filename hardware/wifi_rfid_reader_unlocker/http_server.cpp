90949685f6cf0de21931583b6de04f5ab117dee8
Rada Berar <ujagaga@gmail.com>
2023-05-13 15:04:22 +0200

    Updates

--- hardware/wifi_rfid_reader_unlocker/http_server.cpp
+++ hardware/wifi_rfid_reader_unlocker/http_server.cpp
@@ -16,21 +16,36 @@
 
 
 static const char HTML_BEGIN[] PROGMEM = R"(
-<!DOCTYPE HTML>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-  <head>
-    <meta name = "viewport" content = "width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0">
-    <title>RFID Door Lock</title>
-    <style>
-      body { background-color: white; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }
-      .contain{width: 100%;}.center_div{margin:0 auto; max-width: 400px;position:relative;}
-    </style>
-  </head>
-  <body>
-  <h1>RFID Door Lock</h1>
+<head>
+<meta name = "viewport" content = "width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0">
+<title>RFID Door Lock</title>
+<style>
+  body { background-color: white; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; text-align: center;}
+  .container{width: 100%;}.center_div{margin:0 auto; max-width: 400px;position:relative;}
+  .button{width:200px; padding:5px; border-radius:5px; background-color: orange; text-decoration: none;
+  text-align: center; margin: 10px auto;}
+  .button a{text-decoration: none; color:white;}
+</style>
+</head>
+<body>
+<div class="container">
+<div class="center_div">
+<h1>RFID Lock</h1>
+<p>
 )";
 
-static const char HTML_END[] PROGMEM = "</body></html>";
+static const char HTML_END[] PROGMEM = R"(
+</p>
+<p class="button"><a href="/save_code">Save</a></p>
+<p class="button"><a href="/clear_codes">Clear all codes</a></p>
+</div>
+</div>
+</body>
+</html>
+)";
 
 
 /* Declaring a web server object. */
