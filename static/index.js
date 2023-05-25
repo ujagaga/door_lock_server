c28596b2e091506bc09895ab0c9ca3c122ed281e
Rada Berar <ujagaga@gmail.com>
2023-05-25 14:53:52 +0200

    styling

--- static/index.js
+++ static/index.js
@@ -1,4 +1,13 @@
+var date_choice = document.getElementById("date_choice");
 
 window.onload = function() {
+    date_choice.style.display = "none";
+}
 
+function show_date_choice(){
+    date_choice.style.display = "block";
+}
+
+function close_date_choice(){
+    date_choice.style.display = "none";
 }
